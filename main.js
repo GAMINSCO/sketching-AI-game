2c9f1e5ec30b5ee919606b16d350c7ebc68c1bd9
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2023-01-12 21:00:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 catalogue = ["aircraft carrier", "airplane", "alarm clock", "ambulance", "angel", "animal migration", "ant", "anvil", "apple", "arm", "asparagus", "axe", "backpack", "banana", "bandage", "barn", "baseball", "baseball bat", "basket", "basketball", "bat", "bathtub", "beach", "bear", "beard", "bed", "bee", "belt", "bench", "bicycle", "binoculars", "bird", "birthday cake", "blackberry", "blueberry", "book", "boomerang", "bottlecap", "bowtie", "bracelet", "brain", "bread", "bridge", "broccoli", "broom", "bucket", "bulldozer", "bus", "bush", "butterfly", "cactus", "cake", "calculator", "calendar", "camel", "camera", "camouflage", "campfire", "candle", "cannon", "canoe", "car", "carrot", "castle", "cat", "ceiling fan", "cello", "cell phone", "chair", "chandelier", "church", "circle", "clarinet", "clock", "cloud", "coffee cup", "compass", "computer", "cookie", "cooler", "couch", "cow", "crab", "crayon", "crocodile", "crown", "cruise ship", "cup", "diamond", "dishwasher", "diving board", "dog", "dolphin", "donut", "door", "dragon", "dresser", "drill", "drums", "duck", "dumbbell", "ear", "elbow", "elephant", "envelope", "eraser", "eye", "eyeglasses", "face", "fan", "feather", "fence", "finger", "fire hydrant", "fireplace", "firetruck", "fish", "flamingo", "flashlight", "flip flops", "floor lamp", "flower", "flying saucer", "foot", "fork", "frog", "frying pan", "garden", "garden hose", "giraffe", "goatee", "golf club", "grapes", "grass", "guitar", "hamburger", "hammer", "hand", "harp", "hat", "headphones", "hedgehog", "helicopter", "helmet", "hexagon", "hockey puck", "hockey stick", "horse", "hospital", "hot air balloon", "hot dog", "hot tub", "hourglass", "house", "house plant", "hurricane", "ice cream", "jacket", "jail", "kangaroo", "key", "keyboard", "knee", "knife", "ladder", "lantern", "laptop", "leaf", "leg", "light bulb", "lighter", "lighthouse", "lightning", "line", "lion", "lipstick", "lobster", "lollipop", "mailbox", "map", "marker", "matches", "megaphone", "mermaid", "microphone", "microwave", "monkey", "moon", "mosquito", "motorbike", "mountain", "mouse", "moustache", "mouth", "mug", "mushroom", "nail", "necklace", "nose", "ocean", "octagon", "octopus", "onion", "oven", "owl", "paintbrush", "paint can", "palm tree", "panda", "pants", "paper clip", "parachute", "parrot", "passport", "peanut", "pear", "peas", "pencil", "penguin", "piano", "pickup truck", "picture frame", "pig", "pillow", "pineapple", "pizza", "pliers", "police car", "pond", "pool", "popsicle", "postcard", "potato", "power outlet", "purse", "rabbit", "raccoon", "radio", "rain", "rainbow", "rake", "remote control", "rhinoceros", "rifle", "river", "roller coaster", "rollerskates", "sailboat", "sandwich", "saw", "saxophone", "school bus", "scissors", "scorpion", "screwdriver", "sea turtle", "see saw", "shark", "sheep", "shoe", "shorts", "shovel", "sink", "skateboard", "skull", "skyscraper", "sleeping bag", "smiley face", "snail", "snake", "snorkel", "snowflake", "snowman", "soccer ball", "sock", "speedboat", "spider", "spoon", "spreadsheet", "square", "squiggle", "squirrel", "stairs", "star", "steak", "stereo", "stethoscope", "stitches", "stop sign", "stove", "strawberry", "streetlight", "string bean", "submarine", "suitcase", "sun", "swan", "sweater", "swingset", "sword", "syringe", "table", "teapot", "teddy-bear", "telephone", "television", "tennis racquet", "tent", "The Eiffel Tower", "The Great Wall of China", "The Mona Lisa", "tiger", "toaster", "toe", "toilet", "tooth", "toothbrush", "toothpaste", "tornado", "tractor", "traffic light", "train", "tree", "triangle", "trombone", "truck", "trumpet", "tshirt", "umbrella", "underwear", "van", "vase", "violin", "washing machine", "watermelon", "waterslide", "whale", "wheel", "windmill", "wine bottle", "wine glass", "wristwatch", "yoga", "zebra", "zigzag"]
 
-randomNumber = Math.floor((Math.random() * catalogue.length) + 1)
-console.log(randomNumber)
-
-catalogueItem = catalogue[randomNumber]
-console.log(catalogueItem)
-
-sketch = catalogueItem
-console.log(sketch)
+setSketch()
 
 timerCount = 0;
 timerCheck = ""
@@ -15,16 +8,25 @@ skecthDone = ""
 Answers = ""
 Score = 0;
 
-document.getElementById("next_sketch").innerHTML = "Sketch to be drawn : " + sketch
+function setSketch() {
+    randomNumber = Math.floor((Math.random() * catalogue.length) + 1)
+    console.log(randomNumber)
+
+    window.sketch = catalogue[randomNumber]
+    console.log(sketch)
+        document.getElementById("next_sketch").innerHTML = "Sketch to be drawn : " + sketch
+}
+
 
 function preload() {
-
+    classifier = ml5.imageClassifier('DoodleNet')
 }
 
 function setup() {
     canvas = createCanvas(400, 300)
     canvas.position(485, 340);
     background("white");
+    canvas.mouseReleased(classifyCanvas)
 }
 
 function draw() {
@@ -32,14 +34,15 @@ function draw() {
 
     if (skecthDone == sketch) {
         Answers = "set";
-        Score + 1;
+        Score++
         document.getElementById("Score").innerHTML = "Score : " + Score;
+        console.log("score- ", score)
     }
 
     fill("black")
     strokeWeight(10)
     stroke("black")
-    
+
     if (mouseIsPressed) {
         line(pmouseX, pmouseY, mouseX, mouseY)
     }
@@ -52,12 +55,11 @@ function clear_canvas() {
 function check() {
     timerCount++
     document.getElementById("Timer").innerHTML = "Timer : " + timerCount;
-    console.log(timerCount);
-    if (timerCount > 800) {
+    if (timerCount > 1000) {
         timerCount = 0;
     }
 
-    if (timerCount == 800) {
+    if (timerCount == 1000) {
         timerCheck = "Complete";
     }
 
@@ -65,11 +67,25 @@ function check() {
         timerCheck = ""
         Answers = ""
         updateCanvas()
+        setSketch()
     }
 }
 
 function updateCanvas() {
     background("white");
-    catalogueItem = catalogue[randomNumber]
-    document.getElementById("next_sketch").innerHTML = "Sketch to be drawn " + sketch
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult)
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(result)
+        skecthDone = result[0].label;
+        document.getElementById("label").innerHTML = "Your Sketch : " + skecthDone;
+        document.getElementById("confidence").innerHTML = "Accuracy : " + Math.round(result[0].confidence * 100) + "%";
+    }
 }
